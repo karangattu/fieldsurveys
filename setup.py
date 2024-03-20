64d3c04ea62a92ba482bcf64301760534f4ca280
Karan Gathani <karan.gathani@posit.co>
2024-03-19 21:11:16 -0700

    add debugging instructions for windows issues

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fieldsurveys",
-    version="0.1.35",
+    version="0.1.36",
     author="Karan Gathani",
     author_email="karan.gathani+fieldsurveysapp@posit.co",
     packages=find_packages(),
